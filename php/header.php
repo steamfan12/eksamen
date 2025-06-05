035eb573e7abc86d94b9200c745c79600c159762
Lukas_Mikael <lukasgo@afk.no>
2025-06-05 21:57:52 +0200

    fjernet havgløtt camping

---
+++ php/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <ul>
+            <li><a>Hjem</a></li>
+            <li><a>attraksjoner</a></li>
+            <li><a>login</a></li>
+        </ul>
+    </header>
+</body>
+</html>
--- php/index.php
+++ php/index.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
+
     <div id="container">
-        
+        <h2>Havgløtt camping</h2>
+        <?php include 'header.php';?>
     </div>
 </body>
 </html>
