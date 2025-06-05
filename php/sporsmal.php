e21d3fb3ef6e458614b42910e6dc3d33c005c861
Theo_Helmikstol <theohelmikstol@gmail.com>
2025-06-05 23:01:09 +0200

    fjerna priser laget sporsmal kobla db

---
+++ php/sporsmal.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ofte stilte spørsmål</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+
+    <div id="container">
+        <h2>Havgløtt camping</h2>
+        <?php include 'header.php';?>
+    </div>
+
+    <?php include 'config.php';?>
+</body>
+</html>
